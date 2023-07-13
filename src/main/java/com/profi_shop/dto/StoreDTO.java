64a8e583a84058e39ef50f9a4fa76b631a3e6590
Madanbekov Marsel <madanbekovmarsel@gmail.com>
2023-07-14 01:50:04 +0600

    categories,createProduct,createStore are updated modal frame on submit

--- src/main/java/com/profi_shop/dto/StoreDTO.java
+++ src/main/java/com/profi_shop/dto/StoreDTO.java
@@ -1,2 +1,12 @@
-package com.profi_shop.dto;public class StoreDTO {
+package com.profi_shop.dto;
+
+import lombok.Data;
+
+@Data
+public class StoreDTO {
+    private Long id;
+    private String name;
+    private String town;
+    private String phone_number;
+    private UserDTO admin;
 }
